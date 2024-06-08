b16667120f2706691a4e9ea2f6741e59eb2e0406
MiguelBrav <miguel_98-@hotmail.com>
2024-06-07 22:29:24 -0600

    first conn to cluster mongo

---
+++ FirstConnectionToCluster/Program.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+// A small practice exercise inspired by the MongoDB learning path
+var mongoURL = new MongoUrl("<your connection string>");
+var client = new MongoClient(mongoURL);
+
+var dbList = client.ListDatabases().ToList();
+
+Console.WriteLine("The list of databases on this server is: ");
+
+Array.ForEach(dbList.ToArray(), db =>
+{
+    var dbName = db["name"].AsString;
+    Console.WriteLine($"Database: {dbName}");
+
+    var database = client.GetDatabase(dbName);
+
+    Console.WriteLine("The list of collections on this database is: ");
+
+    var collections = database.ListCollectionNames().ToList();
+
+    Array.ForEach(collections.ToArray(), collectionName =>
+    {
+        Console.WriteLine($"\tCollection: {collectionName}");
+    });
+});
